77e2b55edb7813591b64807534c38aab6df36e03
Olatoye Daramola <daramolatoye@gmail.com>
2021-08-17 14:17:03 +0100

    Update to your code, bro
    Python is fun, shey?
    Lol
    This is the function of your work.
    You're welcome.

--- bmi_calculation.py
+++ bmi_calculation.py
@@ -1,11 +1,4 @@
-weight = input("Enter your weight in pounds:")
-height = input("Enter your height in inche:")
-
-weight_int = int(weight)
-height_int = int(height)
-
-weight_double = weight_per_pounds = 0.453592
-height_double = height_per_meter = 0.0254
-
-bmi = weight_double / height_double * height_double
-print("BMI is:", bmi)
+def BMI(weight,height):
+	return weight / height**2
+	
+print(BMI(0.453592,0.0254))
